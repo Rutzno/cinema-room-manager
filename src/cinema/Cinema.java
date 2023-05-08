02c0a8f7af940f7ada528861078b0a47da4e0078
Rutzno <d.rutzno@gmail.com>
2023-05-08 15:38:01 +0000

    third stage

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -6,17 +6,51 @@ package cinema;
  * @version 1.0.2
  */
 
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
+    static char[][] room;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ticketPrice, fhTicketPrice, lhTicketPrice, totalIncome;
         System.out.print("Enter the number of rows:\n> ");
         int rows = sc.nextInt(); // number of rows
         System.out.print("Enter the number of seats in each row:\n> ");
         int seatsInRow = sc.nextInt(); // number of seats in each row
+
+        initRoom(rows, seatsInRow);
+        display();
+
+        System.out.print("\nEnter a row number:\n> ");
+        int x = sc.nextInt();
+        System.out.print("Enter a seat number in that row:\n> ");
+        int y = sc.nextInt();
+        room[x-1][y-1] = 'B';
+        System.out.printf("%nTicket price: $%d%n", getTicketPrice(x));
+        display();
+//        System.out.printf("Total income: %n$%d", getTotalIncome());
+    }
+
+    private static int getTicketPrice(int x) {
+        int result;
+        int totalSeats = room.length * room[0].length;
+        if (totalSeats < 60) {
+            result = 10;
+        } else {
+            if (x <= room.length/2) {
+                result = 10;
+            } else {
+                result = 8;
+            }
+        }
+        return result;
+    }
+
+    private static int getTotalIncome() {
+        int ticketPrice, fhTicketPrice, lhTicketPrice, totalIncome;
+        int rows = room.length, seatsInRow = room[0].length;
         int totalSeats = rows * seatsInRow;
 
         if (totalSeats < 60) {
@@ -31,20 +65,29 @@ public class Cinema {
                 totalIncome = (fhTicketPrice*(rows/2) + lhTicketPrice*(1+rows/2)) * seatsInRow;
             }
         }
-
-        System.out.printf("Total income: %n$%d", totalIncome);
+        return totalIncome;
     }
 
-    private static void firstStage() {
-        System.out.println("Cinema:");
-        System.out.println("""
-                  1 2 3 4 5 6 7 8
-                1 S S S S S S S S
-                2 S S S S S S S S
-                3 S S S S S S S S
-                4 S S S S S S S S
-                5 S S S S S S S S
-                6 S S S S S S S S
-                7 S S S S S S S S""");
+    private static void initRoom(int rows, int seatsInRow) {
+        room = new char[rows][seatsInRow];
+        for (int i = 0; i < room.length; i++) {
+            Arrays.fill(room[i], 'S');
+        }
+    }
+
+    private static void display() {
+        System.out.println("\nCinema:");
+        System.out.print(" ");
+        for (int i = 0; i < room[0].length; i++) {
+            System.out.print(" " + (i+1));
+        }
+        System.out.println();
+        for (int i = 0; i < room.length; i++) {
+            System.out.print(i+1 + " ");
+            for (int j = 0; j < room[i].length; j++) {
+                System.out.print(room[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
