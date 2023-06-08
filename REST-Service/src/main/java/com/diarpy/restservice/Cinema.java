0587d437a944a3a6ec844cec8afc2d1c5d8648d0
Rutzno <d.rutzno@gmail.com>
2023-06-08 18:55:38 +0000

    REST Service stage 1

---
+++ REST-Service/src/main/java/com/diarpy/restservice/Cinema.java
@@ -0,0 +1,27 @@
+package com.diarpy.restservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cinema {
+    private int totalRows;
+    private int totalColumns;
+    private ArrayList<Seat> availableSeats;
+
+    public Cinema(int totalRows, int totalColumns) {
+        this.totalRows = totalRows;
+        this.totalColumns = totalColumns;
+        availableSeats = new ArrayList<>();
+        for (int i = 0; i < totalRows; i++) {
+            for (int j = 0; j < totalColumns; j++) {
+                availableSeats.add(new Seat(i + 1, j + 1));
+            }
+        }
+    }
+}
