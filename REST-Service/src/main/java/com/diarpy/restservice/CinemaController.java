207081392834cf9f75552783a54420e9d84bb640
Rutzno <d.rutzno@gmail.com>
2023-06-08 19:13:52 +0000

    REST Service stage 2

--- REST-Service/src/main/java/com/diarpy/restservice/CinemaController.java
+++ REST-Service/src/main/java/com/diarpy/restservice/CinemaController.java
@@ -1,9 +1,10 @@
 package com.diarpy.restservice;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Mack_TB
