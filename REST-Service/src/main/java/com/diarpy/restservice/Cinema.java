a165bd1bb0a3246b18f629f18de0bff423c9ef43
Rutzno <d.rutzno@gmail.com>
2023-06-08 19:09:19 +0000

    REST Service stage 2

--- REST-Service/src/main/java/com/diarpy/restservice/Cinema.java
+++ REST-Service/src/main/java/com/diarpy/restservice/Cinema.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 /**
  * @author Mack_TB
  * @since 1/06/2023
- * @version 1.0.11
+ * @version 1.0.22
  */
 
 @Data
@@ -24,10 +24,37 @@ public class Cinema {
         this.totalRows = totalRows;
         this.totalColumns = totalColumns;
         availableSeats = new ArrayList<>();
+        int ticketPrice;
         for (int i = 0; i < totalRows; i++) {
             for (int j = 0; j < totalColumns; j++) {
-                availableSeats.add(new Seat(i + 1, j + 1));
+                ticketPrice = i + 1 <= 4 ? 10 : 8; // determination of ticket price
+                availableSeats.add(new Seat(i + 1, j + 1, ticketPrice));
             }
         }
+    }
+
+    public boolean isSeatWrong(Seat seat) {
+        return seat.getRow() <= 0 || seat.getRow() > totalRows ||
+                seat.getColumn() <= 0 || seat.getColumn() > totalColumns;
+    }
+
+    public boolean isSeatTaken(Seat seat) {
+        for(Seat s: availableSeats) {
+            if (s.getRow() == seat.getRow() && s.getColumn() == seat.getColumn()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Seat bookTicket(Seat seat) {
+        Seat result = null;
+        for(Seat s: availableSeats) {
+            if (s.getRow() == seat.getRow() && s.getColumn() == seat.getColumn()) {
+                result = s;
+            }
+        }
+        availableSeats.remove(result);
+        return result;
     }
 }
