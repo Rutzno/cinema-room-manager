8b90b68d2212308906575019c62784048a8f3cfb
Rutzno <d.rutzno@gmail.com>
2023-06-08 19:00:40 +0000

    REST Service stage 1

--- REST-Service/src/main/java/com/diarpy/restservice/CinemaController.java
+++ REST-Service/src/main/java/com/diarpy/restservice/CinemaController.java
@@ -3,6 +3,12 @@ package com.diarpy.restservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Mack_TB
+ * @since 1/06/2023
+ * @version 1.0.11
+ */
+
 @RestController
 public class CinemaController {
 
