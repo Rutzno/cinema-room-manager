eaf62e4dfe8d717870bf399c8fe21bac0a7b0233
Rutzno <d.rutzno@gmail.com>
2023-06-08 18:58:26 +0000

    REST Service stage 1

---
+++ REST-Service/src/main/java/com/diarpy/restservice/Seat.java
@@ -0,0 +1,13 @@
+package com.diarpy.restservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Seat {
+    private int row;
+    private int column;
+}
