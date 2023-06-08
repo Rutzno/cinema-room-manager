3111a49b35405d81df3349dd48ebf134a4f10aa5
Rutzno <d.rutzno@gmail.com>
2023-06-08 18:58:53 +0000

    REST Service stage 1

--- REST-Service/src/main/java/com/diarpy/restservice/Seat.java
+++ REST-Service/src/main/java/com/diarpy/restservice/Seat.java
@@ -4,6 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author Mack_TB
+ * @since 1/06/2023
+ * @version 1.0.11
+ */
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
