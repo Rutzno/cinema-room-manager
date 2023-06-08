bd4729c4bc2462219dd862ca2e478d4eab974386
Rutzno <d.rutzno@gmail.com>
2023-06-08 18:57:21 +0000

    REST Service stage 1

--- REST-Service/src/main/java/com/diarpy/restservice/RestServiceApplication.java
+++ REST-Service/src/main/java/com/diarpy/restservice/RestServiceApplication.java
@@ -3,6 +3,12 @@ package com.diarpy.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Mack_TB
+ * @since 1/06/2023
+ * @version 1.0.11
+ */
+
 @SpringBootApplication
 public class RestServiceApplication {
 
