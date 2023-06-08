f26ba3d54086f3162d0fb93aac8eb519afee1239
Rutzno <d.rutzno@gmail.com>
2023-06-08 19:11:08 +0000

    REST Service stage 2

--- REST-Service/src/main/java/com/diarpy/restservice/CinemaController.java
+++ REST-Service/src/main/java/com/diarpy/restservice/CinemaController.java
@@ -1,20 +1,31 @@
-package com.diarpy.restservice;
+package cinema;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author Mack_TB
- * @since 1/06/2023
- * @version 1.0.11
- */
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class CinemaController {
+    private Cinema cinema;
 
     @GetMapping("/seats")
     public Cinema getCinema() {
-        return new Cinema(9, 9);
+        cinema = new Cinema(9, 9);
+        return cinema;
     }
 
+    @PostMapping("/purchase")
+    public ResponseEntity<Object> bookTicket(@RequestBody Seat seat) {
+        String error1 = "The number of a row or a column is out of bounds!";
+        String error2 = "The ticket has been already purchased!";
+        if (cinema.isSeatWrong(seat)) {
+            return ResponseEntity.badRequest()
+                    .body(String.format("{\"error\": \"%s\"}", error1));
+        } else if (cinema.isSeatTaken(seat)) {
+            return ResponseEntity.badRequest()
+                    .body(String.format("{\"error\": \"%s\"}", error2));
+        }
+        return ResponseEntity.ok(cinema.bookTicket(seat));
+    }
 }
