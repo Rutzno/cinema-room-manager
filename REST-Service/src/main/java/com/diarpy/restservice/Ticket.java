eb0a31f9410add77140d29b575b93ec363a6acf3
Rutzno <d.rutzno@gmail.com>
2023-06-09 11:18:29 +0000

    REST Service stage 3

--- REST-Service/src/main/java/com/diarpy/restservice/Ticket.java
+++ REST-Service/src/main/java/com/diarpy/restservice/Ticket.java
@@ -4,6 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author Mack_TB
+ * @since 1/06/2023
+ * @version 1.0.33
+ */
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
