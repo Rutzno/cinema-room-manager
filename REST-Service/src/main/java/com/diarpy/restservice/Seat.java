df36d6a2937f250741737654f0c871aa809e2a71
Rutzno <d.rutzno@gmail.com>
2023-06-09 11:19:27 +0000

    REST Service stage 3

--- REST-Service/src/main/java/com/diarpy/restservice/Seat.java
+++ REST-Service/src/main/java/com/diarpy/restservice/Seat.java
@@ -4,17 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 /**
  * @author Mack_TB
  * @since 1/06/2023
- * @version 1.0.22
+ * @version 1.0.33
  */
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Seat {
-    private int row;
-    private int column;
-    private int price;
+    private UUID token;
+    private Ticket ticket;
 }
